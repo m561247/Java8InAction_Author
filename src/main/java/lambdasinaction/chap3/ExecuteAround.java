ff7ad9769d07b5700686a79f04d474dd6e7b8043
m561247 <56660879+m561247@users.noreply.github.com>
2020-11-03 19:10:33 +0800

    no message

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -7,33 +7,34 @@ public class ExecuteAround {
 
         // method we want to refactor to make more flexible
         String result = processFileLimited();
-        System.out.println(result);
+        System.out.println("result = " + result);
 
         System.out.println("---");
 
 		String oneLine = processFile((BufferedReader b) -> b.readLine());
-		System.out.println(oneLine);
+		System.out.println("oneLine = " + oneLine);
 
 		String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
-		System.out.println(twoLines);
+		System.out.println("twoLines = " + twoLines);
 
 	}
 
     public static String processFileLimited() throws IOException {
         try (BufferedReader br =
-                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+                     new BufferedReader(new FileReader("C:\\ThinkingInJava_workspace\\J8InAction\\src\\main\\resources\\lambdasinaction\\chap3\\data.txt"))) {
             return br.readLine();
         }
     }
 
 
 	public static String processFile(BufferedReaderProcessor p) throws IOException {
-		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
+		try(BufferedReader br = new BufferedReader(new FileReader("C:\\ThinkingInJava_workspace\\J8InAction\\src\\main\\resources\\lambdasinaction\\chap3\\data.txt"))){
 			return p.process(br);
 		}
 
 	}
-
+	
+	@FunctionalInterface
 	public interface BufferedReaderProcessor{
 		public String process(BufferedReader b) throws IOException;
 
